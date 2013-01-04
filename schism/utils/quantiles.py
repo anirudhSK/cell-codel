9526f77aa7356a73d858ec7ee22db7a35ef8194e
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-04 12:56:17 -0500

    Make quantiles memory friendly by reading serially; avoid sorting.

--- schism/utils/quantiles.py
+++ schism/utils/quantiles.py
@@ -1,21 +1,33 @@
 #! /usr/bin/python
 import sys
 fh=sys.stdin
-delay_list=[]
-for line in fh.readlines() :
+delay_dict=dict()
+cdf=dict()
+for line in fh :
 	line.strip();
 	if len(line.split()) < 10 :
 		continue;
 	flow_id=int(line.split()[5])
 	if flow_id == int(sys.argv[1]) :
 		delay=int(line.split()[3].strip(','))
-		delay_list.append(delay)
-delay_list.sort()
-i=0
-for i in range(1,100,1) :
-	print delay_list[int(i*0.01*len(delay_list))]," ",i*0.01
-print>>sys.stderr,"median ",delay_list[int(0.50*len(delay_list))]
-print>>sys.stderr,"95th percentile ",delay_list[int(0.95*len(delay_list))]
-print>>sys.stderr,"99th percentile ",delay_list[int(0.99*len(delay_list))]
-print>>sys.stderr,"max ",delay_list[-1]
-print>>sys.stderr,"length,",len(delay_list)
+		if delay in delay_dict :
+			delay_dict[delay] += 1
+		else :
+			delay_dict[delay] = 1
+delay_list = delay_dict.keys()
+delay_list.sort() 
+
+acc = 0;
+total_length = float( sum( delay_dict.values() ) )
+for delay in delay_list :
+	acc += delay_dict[delay]
+	cdf[delay] = acc/total_length ;
+
+for delay in delay_list :
+	print delay," ",cdf[delay]
+
+print>>sys.stderr,"median ",[ delay for delay in delay_list if cdf[delay] >= 0.5 ][0];
+print>>sys.stderr,"95th percentile ",[ delay for delay in delay_list if cdf[delay] >= 0.95 ][0];
+print>>sys.stderr,"99th percentile ",[ delay for delay in delay_list if cdf[delay] >= 0.99 ][0];
+print>>sys.stderr,"max ",delay_list[-1];
+print>>sys.stderr,"length,",total_length
