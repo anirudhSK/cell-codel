5f74c1c63b610166c5e4f45e38885d04e672902a
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-09 12:53:53 -0500

    Pick new burst size on every tick.

--- schism/src/slotted-sender.cc
+++ schism/src/slotted-sender.cc
@@ -15,12 +15,9 @@ SlottedSender::SlottedSender( int32_t flow_id, double rate, int seed, int batch_
 std::vector<Packet> SlottedSender::tick( uint64_t current_tick )
 {
 	_tick=current_tick;
-	if ( _tick % 1000 == 0 ) {
-		_current_batch = _batch_size.sample() ;
-		auto batch_rate = _rate/_current_batch;
-		fprintf( stderr, "Tick %lu batch rate -> %f, batch size %d \n", _tick, batch_rate, _current_batch );
-		_arrivals = Poisson( batch_rate, _seed );
-	}
+	_current_batch = _batch_size.sample() ;
+	auto batch_rate = _rate/_current_batch;
+	_arrivals = Poisson( batch_rate, _seed );
 	return generate_packets();
 }
 
