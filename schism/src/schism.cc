71e03d032c5870c7e9c971f88165cc22961f9e7c
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-09 12:58:28 -0500

    Take in burst size from command line.

--- schism/src/schism.cc
+++ schism/src/schism.cc
@@ -1,28 +1,29 @@
 #include "slotted-link.hh"
 #include "slotted-sender.hh"
 #include "scheduler.hh"
-#include "scheduling-algms/rr-scheduler.hh"
+#include "scheduling-algms/tail-scheduler.hh"
 #include <assert.h>
 
 int main( int argc, char* argv[] )
 {
-	assert (argc == 3);
+	assert (argc == 4);
 	int seed  = atoi( argv[1] );
-	bool skew = ( std::string(argv[2]) == "skew" ) ;
-	
+	int batch_size = atoi( argv[2] );
+	float ingress_rate = atof( argv[3] );
 	/* Pick a Scheduler */
-	Scheduler * scheduler = new RRScheduler();
+	Scheduler * scheduler = new TailScheduler();
 	
 	/* Next sender */
 	std::vector<SlottedSender *> sender_list;
 
-	/* pick 10 senders */
-	int N = 10;
-	int batch_size = 1;
+	/* pick 500 senders */
+	int N = 500;
+	uint32_t num_ticks = 100000;
 	int i = 0;
+	fprintf( stderr, "Using N = %d  senders, ingress rate = %f, batch size %d, running for %u ticks \n", N, ingress_rate, batch_size, num_ticks);
 	for ( i=0; i<N; i++ )
 	{
-		float rate = skew ?  (i*0.99/45.0) + 1e-30 :  0.099 + 1e-30;
+		float rate = ingress_rate/N ;
 		fprintf(stderr,"Using rate %f \n",rate);
 		SlottedSender* next_sender = new SlottedSender( i, rate, seed, batch_size );
 		sender_list.push_back( next_sender );
@@ -32,7 +33,7 @@ int main( int argc, char* argv[] )
 	SlottedLink link( scheduler, seed );
 	
 	uint64_t current_tick=0;
-	for ( current_tick=0; current_tick < 1000000; current_tick++ )
+	for ( current_tick=0; current_tick < num_ticks; current_tick++ )
 	{
 		link.tick(current_tick);
 		std::vector<Packet> new_pkts;
