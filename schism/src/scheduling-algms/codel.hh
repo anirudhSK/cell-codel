ef68b3914df25c37a8cc8ff26747636b8082c76e
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-01-11 11:43:50 -0500

    Remove unnecessary parts of DelayQueue and rename class to CoDel.

--- schism/src/scheduling-algms/codel.hh
+++ schism/src/scheduling-algms/codel.hh
@@ -1,5 +1,5 @@
-#ifndef DELAY_QUEUE_HH
-#define DELAY_QUEUE_HH
+#ifndef CODEL_HH
+#define CODEL_HH
 #include <unistd.h>
 #include <string>
 #include <stdio.h>
@@ -7,12 +7,11 @@
 #include <queue>
 #include <limits.h>
 #include <list>
-#include "timestamp.h"
 #include <math.h>
+#include "../packet.hh"
 using namespace std;
-/* Codel parameter */
 
-class DelayQueue
+class CoDel
 {
 private:
   class DelayedPacket
@@ -21,10 +20,9 @@ private:
     uint64_t entry_time;
     uint64_t release_time;
     string contents;
-    uint16_t bytes_earned;
 
     DelayedPacket( uint64_t s_e, uint64_t s_r, const string & s_c )
-      : entry_time( s_e ), release_time( s_r ), contents( s_c ) , bytes_earned(0) {}
+      : entry_time( s_e ), release_time( s_r ), contents( s_c ) {}
   };
 
   typedef struct {
@@ -32,59 +30,35 @@ private:
        bool ok_to_drop;
   } dodeque_result; 
 
-  static const int SERVICE_PACKET_SIZE = 1500;
-
-  uint64_t convert_timestamp( const uint64_t absolute_timestamp ) const { return absolute_timestamp - _base_timestamp; }
-
-  const string _name;
-
-  std::queue< DelayedPacket > _delay;
-  std::queue< DelayedPacket > _pdp;
-
-  std::queue< uint64_t > _schedule;
-
-  std::vector< string > _delivered;
-
-  const uint64_t _ms_delay;
-
-  uint64_t _total_bytes;
-  uint64_t _used_bytes;
-
-  uint64_t _queued_bytes;
-  uint64_t _bin_sec;
-
-  uint64_t _base_timestamp;
-
-  void tick( void );
+  /* Flow Queue that Codel is tracking */
+  std::queue<Packet> _flow_queue;
+  uint32_t _flow_id;
 
   /* Codel - specific parameters */
   static const uint64_t    target=5 ;    /* 5   ms as per the spec */
   static const uint64_t  interval=100;   /* 100 ms as per the spec */
-  static const uint16_t maxpacket=1500;    /* MTU of the link */
+  static const uint16_t maxpacket=1;    /* MTU of the link, 1 in our tick simulator */
 
   /* Codel - specific tracker variables */
   uint64_t first_above_time;
   uint64_t drop_next;
   uint32_t count;
   bool     dropping;
-  uint32_t _bytes_in_queue;
   uint32_t drop_count; /* for statistics */
-  bool _codel_enabled;
+
   /* Codel - control rule */
   uint64_t control_law (uint64_t t) { return t + interval/sqrt(count);}
+
   /* Codel - reqd. deq functions */
-  DelayedPacket _pdp_deq();
+  DelayedPacket _codel_deq();
 
   /* Main codel routines */
-  dodeque_result dodeque ( void );
-  DelayedPacket deque ( void ); 
+  dodeque_result dodeque ( uint64_t now );
+  DelayedPacket deque ( uint64_t now );
   void drop ( DelayedPacket p);
 
 public:
-  DelayQueue( const string & s_name, const uint64_t s_ms_delay, const char *filename, const uint64_t base_timestamp , bool codel_enable );
+  CoDel( std::queue<Packet> & flow_queue, uint32_t flow_id );
 
-  int wait_time( void );
-  std::vector< string > read( void );
-  void write( const string & packet );
 };
 #endif
