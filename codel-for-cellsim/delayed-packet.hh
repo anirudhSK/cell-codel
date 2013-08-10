4e6c2968efd6dfc66c13a8169cca43363e3ed52f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-08-10 17:27:25 -0400

    Add ifdef guard, create a nullpkt() function

--- codel-for-cellsim/delayed-packet.hh
+++ codel-for-cellsim/delayed-packet.hh
@@ -1,3 +1,6 @@
+#ifndef DELAYED_PACKET_HH_
+#define DELAYED_PACKET_HH_
+
 #include <string>
 
 class DelayedPacket
@@ -10,4 +13,8 @@ public:
 
   DelayedPacket( uint64_t s_e, uint64_t s_r, const std::string & s_c )
     : entry_time( s_e ), release_time( s_r ), contents( s_c ) , bytes_earned(0) {}
+
+  static DelayedPacket nullpkt() { return DelayedPacket( -1, -1, ""); }
 };
+
+#endif  // DELAYED_PACKET_HH_
