07443f99efa940c0d5c3792fbe3960990bcf5405
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-09-04 12:29:03 +0000

    Modify cellsim-runner to run cell-sched.

--- cellsim-runner.sh
+++ cellsim-runner.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /home/ubuntu/cell-codel/codel-for-cellsim/cell-codel-sim /home/ubuntu/verizon_lte_uplink_slice.ms /home/ubuntu/verizon_lte_downlink_slice.ms 00:00:00:00:00:02 0 LTE-eth0 LTE-eth1 >/tmp/cellsim-stdout 2>/tmp/cellsim-stderr
+exec /home/ubuntu/cell-codel/codel-for-cellsim/cell-sched /home/ubuntu/verizon_lte_uplink_slice.ms /home/ubuntu/verizon_lte_downlink_slice.ms 00:00:00:00:00:02 0 LTE-eth0 LTE-eth1 >/tmp/cellsim-stdout 2>/tmp/cellsim-stderr
