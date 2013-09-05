6224080d1d6a057801b0127a087655d1242e43ee
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-09-05 12:22:28 +0000

    Add qdisc to cellsim-runner.sh

--- cellsim-runner.sh
+++ cellsim-runner.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-exec /home/ubuntu/cell-codel/codel-for-cellsim/cell-sched /home/ubuntu/verizon_lte_uplink_slice.ms /home/ubuntu/verizon_lte_downlink_slice.ms 00:00:00:00:00:02 0 LTE-eth0 LTE-eth1 >/tmp/cellsim-stdout 2>/tmp/cellsim-stderr
+if [ $# -lt 1 ]; then
+  echo "Enter qdisc"
+  exit
+fi;
+qdisc=$1
+exec /home/ubuntu/cell-codel/codel-for-cellsim/cell-sched /home/ubuntu/verizon_lte_uplink_slice.ms /home/ubuntu/verizon_lte_downlink_slice.ms 00:00:00:00:00:02 $qdisc LTE-eth0 LTE-eth1 >/tmp/cellsim-stdout 2>/tmp/cellsim-stderr
