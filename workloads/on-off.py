9a5d3596f6e9f99f4689184ef048bdfd58918cc3
Anirudh Sivaraman <sk.anirudh@gmail.com>
2013-09-05 18:21:34 +0000

    Log exceptions and requests to per-thread logs instead of stderr

--- workloads/on-off.py
+++ workloads/on-off.py
@@ -5,9 +5,10 @@ from time import time
 from time import sleep
 import random
 import thread
+from httplib import BadStatusLine
 
 # Fetch one flow's worth of bytes
-def issue_next_get(flow_size, conn, is_persistent):
+def issue_next_get(flow_size, conn, is_persistent, thread_fh):
   time_before_get = time()
   if (is_persistent):
     conn.request("GET", "/randomhuge", '', {'Range' : 'bytes=0'+'-'+str(flow_size - 1)})
@@ -15,16 +16,22 @@ def issue_next_get(flow_size, conn, is_persistent):
     conn.request("GET", "/randomhuge", '', {'Range' : 'bytes=0'+'-'+str(flow_size - 1), 'Connection' : 'Close'})
 
   time_after_get  = time()
-  print >> sys.stderr, "request string\nGET /randomhuge\nRange : bytes=0"+"-"+str(flow_size - 1);
-  resp = conn.getresponse()
-  resp.read()
+  print >> thread_fh, "request string\nGET /randomhuge\nRange : bytes=0" + "-" + str(flow_size - 1);
+  thread_fh.flush()
+  resp=''
+  try:
+    resp = conn.getresponse()
+    resp.read()
+  except BadStatusLine as e:
+    print >> thread_fh, e.line, e.args
+    thread_fh.flush()
   time_to_response = time()
   syn_fct = int(1000*(time_to_response-time_before_get))
   get_fct = int(1000*(time_to_response-time_after_get))
   return (syn_fct, get_fct)
 
 # Simulate an ON/OFF source
-def on_off_source(thread_name, url, duration, persistent_str, conns, time_start):
+def on_off_source(thread_name, url, duration, persistent_str, conns, time_start, thread_fh):
   print thread_name
   # Initialization
   current_time = time() - time_start;
@@ -49,7 +56,7 @@ def on_off_source(thread_name, url, duration, persistent_str, conns, time_start)
       flow_size = int(1e9);
 
     # Retrieve flow
-    (syn_fct, get_fct) = issue_next_get(flow_size, conns[thread_name], is_persistent)
+    (syn_fct, get_fct) = issue_next_get(flow_size, conns[thread_name], is_persistent, thread_fh)
  
     # print stats
     print thread_name," Flow #", counter," size: ", flow_size," bytes syn_fct: ", syn_fct," get_fct: ", get_fct 
@@ -82,6 +89,7 @@ if __name__ == "__main__" :
   conns = dict()
   print "Initializing random seed", random_seed
   random.seed(random_seed)
-  for i in range(0, nsrc):    
-    thread.start_new_thread(on_off_source, ("Thread-"+str(i), url, duration, persistent_str, conns, time_start))
+  for i in range(0, nsrc):
+    thread_fh = open("/tmp/" + str(i) + ".threadlog", "w");
+    thread.start_new_thread(on_off_source, ("Thread-"+str(i), url, duration, persistent_str, conns, time_start, thread_fh))
   sleep(duration + 1)
